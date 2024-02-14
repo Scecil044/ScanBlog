9a33e9360fc855e6ace21f1bb88a3dd3c77a6d08
cecilDev001 <cecilspencer50@gmail.com>
2024-02-14 15:21:07 +0300

    created header section with responsive navbar from flowbite

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,10 @@ import Header from "./components/Header";
 import Profile from "./pages/Profile";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import DashBoard from "./pages/DashBoard";
+import Projects from "./pages/Projects";
+import NotFound from "./pages/NotFound";
+import About from "./pages/About";
 
 export default function App() {
   return (
@@ -15,6 +19,10 @@ export default function App() {
           <Route path="/profile" element={<Profile />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/dashboard" element={<DashBoard />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
     </>
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,58 @@
-import { Navbar } from "flowbite-react";
+import { Button, Navbar, TextInput } from "flowbite-react";
+import { AiOutlineSearch } from "react-icons/ai";
+import { FaRegMoon } from "react-icons/fa6";
+
+import { Link, useLocation } from "react-router-dom";
 
 export default function Header() {
+  const path = useLocation().pathname;
   return (
     <>
-      <Navbar></Navbar>
+      <Navbar className="border-b-2">
+        <Link
+          to="/"
+          className="flex items-center text-sm sm:text-xl text-nowrap"
+        >
+          <span className="px-2 py-1 text-white bg-gradient-to-r from-indigo-500 via bg-purple-500 to-pink-500 rounded-lg">
+            TheScience
+          </span>
+          Blog
+        </Link>
+
+        <form className="hidden lg:inline">
+          <TextInput
+            type="text"
+            placeholder="Search..."
+            rightIcon={AiOutlineSearch}
+          />
+        </form>
+        <Button pill className="w-12 h-10 lg:hidden" color="gray">
+          <AiOutlineSearch />
+        </Button>
+
+        <div className="flex gap-2 md:order-2">
+          <Button color="gray" className="hidden sm:inline">
+            <FaRegMoon />
+          </Button>
+          <Link to="/login">
+            <Button color="grey" gradientDuoTone="purpleToBlue">
+              Sign In
+            </Button>
+          </Link>
+          <Navbar.Toggle />
+        </div>
+        <Navbar.Collapse>
+          <Navbar.Link active={path === "/"} as={"div"}>
+            <Link to="/">Home</Link>
+          </Navbar.Link>
+          <Navbar.Link active={path === "/about"} as={"div"}>
+            <Link to="/about">About</Link>
+          </Navbar.Link>
+          <Navbar.Link active={path === "/projects"} as={"div"}>
+            <Link to="/projects">Projects</Link>
+          </Navbar.Link>
+        </Navbar.Collapse>
+      </Navbar>
     </>
   );
 }

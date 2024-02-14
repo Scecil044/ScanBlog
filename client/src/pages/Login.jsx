1d78f0424f42ce4ecd50866672f1f226070bc042
cecilDev001 <cecilspencer50@gmail.com>
2024-02-14 17:12:43 +0300

    completed register component

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,3 +1,55 @@
+import { Link } from "react-router-dom";
+import { TextInput, Label, Button } from "flowbite-react";
+
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <div className="min-h-screen">
+      <div className="flex md:items-center flex-col gap-3 md:flex-row md:gap-5 md:p-5 mt-20 max-w-3xl mx-auto p-3">
+        {/* left */}
+        <div className="flex-1">
+          <Link to="/" className="flex items-center text-4xl font-bold">
+            <span className="text-white py-3 px-4 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg">
+              TheScience
+            </span>
+            Blog
+          </Link>
+          <p className="text-sm mt-5">
+            This is a demo application. It has been built on React, node,
+            express, and the elgenance of Tailwind
+          </p>
+        </div>
+        {/* right */}
+        <div className="flex-1">
+          <form className="flex flex-col">
+            <div className="mb-4">
+              <Label value="First Name" />
+              <TextInput type="text" placeholder="First Name" id="firstName" />
+            </div>
+
+            <div className="mb-4">
+              <Label value="Last Name" />
+              <TextInput type="text" placeholder="Last Name" id="lastName" />
+            </div>
+
+            <div className="mb-4">
+              <Label value="Email" />
+              <TextInput
+                type="email"
+                placeholder="someone@gmail.com"
+                id="email"
+              />
+            </div>
+
+            <div className="mb-4">
+              <Label value="Password" />
+              <TextInput type="password" placeholder="Password" id="password" />
+            </div>
+            <Button type="submit" gradientDuoTone="purpleToPink">
+              Sign Up
+            </Button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,3 +1,109 @@
+import { Button, Label, TextInput } from "flowbite-react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  authFulfilledState,
+  authPendingState,
+  authRejectedState,
+} from "../redux/authSlice";
+
 export default function Register() {
-  return <div>Register</div>;
+  const { isLoading, isError } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({});
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch(authPendingState());
+      const res = await fetch("/api/auth/register", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(authRejectedState(data.message));
+        return;
+      }
+      dispatch(authFulfilledState(data));
+      navigate("/");
+    } catch (error) {
+      dispatch(authRejectedState(error.message));
+    }
+  };
+  return (
+    <div className="min-h-screen">
+      <div className="flex md:items-center flex-col gap-3 md:flex-row md:gap-5 md:p-5 mt-20 max-w-3xl mx-auto p-3">
+        {/* left */}
+        <div className="flex-1">
+          <Link to="/" className="flex items-center text-4xl font-bold">
+            <span className="text-white py-3 px-4 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg">
+              TheScience
+            </span>
+            Blog
+          </Link>
+          <p className="text-sm mt-5">
+            This is a demo application. It has been built on React, node,
+            express, and the elgenance of Tailwind
+          </p>
+        </div>
+        {/* right */}
+        <div className="flex-1">
+          <form onSubmit={handleSubmit} className="flex flex-col">
+            <div className="mb-4">
+              <Label value="First Name" />
+              <TextInput
+                type="text"
+                placeholder="First Name"
+                id="firstName"
+                onChange={handleChange}
+              />
+            </div>
+
+            <div className="mb-4">
+              <Label value="Last Name" />
+              <TextInput
+                type="text"
+                placeholder="Last Name"
+                id="lastName"
+                onChange={handleChange}
+              />
+            </div>
+
+            <div className="mb-4">
+              <Label value="Email" />
+              <TextInput
+                type="email"
+                placeholder="someone@gmail.com"
+                id="email"
+                onChange={handleChange}
+              />
+            </div>
+
+            <div className="mb-4">
+              <Label value="Password" />
+              <TextInput
+                type="password"
+                placeholder="Password"
+                id="password"
+                onChange={handleChange}
+              />
+            </div>
+            <Button type="submit" gradientDuoTone="purpleToPink">
+              Sign Up
+            </Button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
